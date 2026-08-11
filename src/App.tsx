629b44e02a440b6c0b619c6293d8085fb2ed82e0
Graham <gcoulby@googlemail.com>
2026-08-11 21:51:51 +0100

    tabs, with beatboard tab

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,42 @@ import StatusBar from '@/components/status-bar'
 import { useEditorStore } from './stores/editorStore'
 import { useGlobalShortcuts } from '@/hooks/useGlobalShortcuts'
 import AppNavigator from './components/left-side-panel/AppNavigator'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs'
+import { PencilLine, SquareKanban } from 'lucide-react'
+import BeatBoard from './components/screens/beat-board'
 function App() {
   const editor = useEditorStore((s) => s.editor)
   useGlobalShortcuts()
   return (
     <main className="w-dvw h-dvh overflow-hidden">
       <HeaderPanel />
-      <div className="flex flex-row">
+      <div className="flex flex-row gap-0!">
         <AppNavigator editor={editor}>
-          <ScreenplayEditor />
+          <Tabs defaultValue="editor" className="gap-0 bg-background">
+            <TabsList className="h-(--tabbar-h)! p-0 gap-2 bg-background">
+              <TabsTrigger
+                value="editor"
+                className="data-active:bg-primary data-active:text-primary-foreground"
+              >
+                <PencilLine />
+                Editor
+              </TabsTrigger>
+              <TabsTrigger value="beat-board">
+                <SquareKanban />
+                Beat Board
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="editor">
+              <div className="h-(--app-h)! w-full overflow-hidden">
+                <ScreenplayEditor />
+              </div>
+            </TabsContent>
+            <TabsContent value="beat-board">
+              <div className="h-(--app-h)! w-full overflow-hidden">
+                <BeatBoard />
+              </div>
+            </TabsContent>
+          </Tabs>
           <StatusBar editorDoc={editor?.getJSON()} />
         </AppNavigator>
       </div>
